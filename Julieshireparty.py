0bfd82db93cacc635e4f21754e8b8d5e0c323782
st22052 <st22052@ormiston.school.nz>
2023-05-31 17:26:01 +1200

    Update Julieshireparty.py

--- Julieshireparty.py
+++ Julieshireparty.py
@@ -7,16 +7,6 @@ window.title("Julie's hire party")
 window.geometry("1100x600") 
 window.configure(bg="white")
 window.attributes('-fullscreen',True)
-# Add shapes
-entry_box_bg = tk.Label(window, width=30, height=17,fg="red",bg="#213A5C")
-entry_box_bg.place(x=433,y=190)
-
-label_bg = tk.Label(window, width=30, height=17,fg="red",bg="#213A5C")
-label_bg.place(x=200,y=190)
-
-button_bg = tk.Label(window, width=64, height=10, bg="#213A5C")
-button_bg.place(x=200,y=465)
-
 
 # Create custom table
 table_frame = tk.Frame()
@@ -35,33 +25,51 @@ entry_frame = ttk.Frame()
 entry_frame.pack(pady=10)
 
 
+# Add shapes
+entry_box_bg = tk.Label(window, width=40, height=17,fg="red",bg="#213A5C")
+entry_box_bg.place(x=393,y=190)
+
+label_bg = tk.Label(window, width=40, height=17,fg="red",bg="#213A5C")
+label_bg.place(x=90,y=190)
+
+button_bg = tk.Label(window, width=84, height=10, bg="#213A5C")
+button_bg.place(x=90,y=465)
+
+
+
 #Headings for table
 for col in columns:
     treeview.heading(col, text=col)
 
+
 #create function to add data to the table
 def add_data():
-    Name = customer_name.get()
     try:
         Receipt = int(receipt_num.get())
     except ValueError:
-        receipt_error.config(text="Please enter a number")
+        receipt_error.config(text="Please enter receipt number")
 
+    Name = customer_name.get()
     Item_Hired = item_hired.get()
     Number_Hired = num_hired.get()
     
     # Validation checks
+    if not Name.strip():
+        customer_name_error.config(text="Name Required")
+        return # Exit the function if Name is empty
+
     if not Item_Hired.strip():
         item_hired_error.config(text="Item Hired cannot be empty")
         return  # Exit the function if Item_Hired is empty
 
     if not Number_Hired.isdigit() or int(Number_Hired) < 1 or int(Number_Hired) > 500:
-        num_error.config(text="Items Hired must be a number between 1-500")
-         # Exit the function if Number_Hired is empty
+        num_error.config(text="1-500 Items only")
+        return # Exit the function if Number_Hired is empty
 
     # Clear any previous error messages
     item_hired_error.config(text="")
     num_error.config(text="")
+    customer_name_error.config(text="")
     # Print to terminal
     print("Name: ", Name)
     print("Item hired: ", Item_Hired)
@@ -72,14 +80,19 @@ def add_data():
     treeview.insert("", "end", values=(Name, Receipt, Item_Hired, Number_Hired))
 
 
-item_hired_error = Label(entry_box_bg, text='', bg="yellow")
-item_hired_error.place(x=10,y=135)
+customer_name_error = Label(entry_box_bg, bg="yellow")
+customer_name_error.place(x=50,y=35)
 
-num_error = Label(entry_box_bg, text='', bg="yellow")
-num_error.place(x=10,y=185)
+receipt_error = Label(entry_box_bg, bg="yellow")
+receipt_error.place(x=50,y=85)
 
-receipt_error = Label(entry_box_bg, text='', bg="yellow")
-receipt_error.place(x=10,y=85)
+item_hired_error = Label(entry_box_bg, bg="yellow")
+item_hired_error.place(x=50,y=135)
+
+num_error = Label(entry_box_bg, bg="yellow")
+num_error.place(x=50,y=185)
+
+
 
 # Functions and buttons to delete data off of the table
 
@@ -103,9 +116,9 @@ def delete_all():
 #edit_btn = ttk.Button(window, text="Edit", width=23, command=edit)
 #edit_btn.place(x=1000,y=440)
 del_btn = ttk.Button(button_bg, text="Delete selected row", width=23, command=delete)
-del_btn.place(x=250,y=100)
+del_btn.place(x=350,y=100)
 delete_all = ttk.Button(button_bg, text="Delete all", width=23, command=delete_all)
-delete_all.place(x=50,y=100)
+delete_all.place(x=100,y=100)
 
 
 # Create buttons to add to table
@@ -113,7 +126,7 @@ delete_all.place(x=50,y=100)
 # submit button
 append_button = tk.Button(button_bg, text="Append Details & Print",font=("Arial",12,"bold"), command=add_data)
 append_button.pack(padx=10,pady=300)
-append_button.place(x=130,y=10)
+append_button.place(x=200,y=20)
 
 
 #btn_print = tk.Button(window,text="Print Details",font=("Arial",12,"bold"), command=add_data)
@@ -127,17 +140,17 @@ btn_exit.place(x=1360,y=760)
 
 
 # Labels
-lb_header = tk.Label(window,text= "Julie's Party",font=('Arial',45,"bold"), width= 23,fg="red",bg="lightgrey")
+lb_header = tk.Label(window,text= "Julie's Party",font=('Arial',45,"bold"), width= 20,fg="red",bg="lightgrey")
 lb_name = tk.Label(label_bg,text= "Customer Name",font=('Arial',14),fg="black",bg="lightgrey")
 lb_receipt = tk.Label(label_bg,text= "Receipt Number",font=('Arial',14),fg="black",bg="lightgrey")
 lb_item = tk.Label(label_bg,text= "Item Hired",font=('Arial',14),fg="black",bg="lightgrey")
 lb_num = tk.Label(label_bg,text= "Quantity of items",font=('Arial',14),fg="black",bg="lightgrey")
 # Label positions
 lb_header.place(x=0,y=0)
-lb_name.place(x=35,y=9)
-lb_receipt.place(x=35,y=50)
-lb_item.place(x=35,y=100)
-lb_num.place(x=35,y=155)
+lb_name.place(x=70,y=9)
+lb_receipt.place(x=70,y=50)
+lb_item.place(x=70,y=100)
+lb_num.place(x=70,y=155)
 
 
 # Create entry boxes
